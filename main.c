91dca03edbd4cb93d98aa138a922631a6b9058b4
Dylan Gillespie <dylantannergillespie@gmail.com>
2023-04-26 11:47:19 -0400

    Add project files.

---
+++ main.c
@@ -0,0 +1,10 @@
+#pragma comment(lib, "legacy_stdio_definitions.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "user32.lib")
+
+#include <GLFW/glfw3.h>
+
+int main() {
+	return 0;
+}
