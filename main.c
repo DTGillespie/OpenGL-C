5b7c867445cf73206616f4d33e5b90fa1d00a6da
Dylan Gillespie <dylantannergillespie@gmail.com>
2023-04-26 12:23:44 -0400

    OpenGL window initialized

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "opengl32.lib")
 
+#include <stdio.h>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main() {
 
@@ -12,5 +15,37 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL-C", NULL, NULL);
+	if (window == NULL) {
+		printf("Failed to initialize GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	int version = gladLoadGL(glfwGetProcAddress);
+	if (version == 0) {
+		printf("Failed to initialize GLAD\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	printf("Successfully Initialized OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
